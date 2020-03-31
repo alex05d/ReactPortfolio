ce9e8391ce81582f7b977658f0d546d54147244d
Alexiss Diaz <alexiss.diaz@live.com>
2020-03-31 17:26:39 -0500

    Updates

---
+++ precache-manifest.ab85586384ec41d3c3e0de7d6c2cd94e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77f052624c6984113ae3",
+    "url": "/react_portfolio/static/css/main.052987d0.chunk.css"
+  },
+  {
+    "revision": "77f052624c6984113ae3",
+    "url": "/react_portfolio/static/js/main.77f05262.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react_portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "c37a5afe0dc8109e461b",
+    "url": "/react_portfolio/static/js/runtime~main.c37a5afe.js"
+  },
+  {
+    "revision": "abb42f081889027d1fa69423d9f9d71f",
+    "url": "/react_portfolio/index.html"
+  }
+];
