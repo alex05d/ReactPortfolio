fc237bdcc1952efd5b5052f654372ecf6a11536d
Alexiss Diaz <alexiss.diaz@live.com>
2020-03-31 16:29:12 -0500

    added react portfolio files

---
+++ src/resumeData.js
@@ -0,0 +1,153 @@
+let resumeData = {
+  "imagebaseurl": "www.linkedin.com/in/alexiss-diaz-9b15b818b",
+  "name": "Alexiss Diaz",
+  "role": "Full Stack Developer",
+  "linkedinId": " www.linkedin.com/in/alexiss-diaz-9b15b818b",
+  "skypeid": "",
+  "roleDescription": "I like dabbling in various parts of frontend and backend development. I like to learn about new technologies, take fishing trips or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/alexiss-diaz-9b15b818b",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/alex05d",
+      "className": "fa fa-github"
+    },
+  ],
+  "aboutme": "I am a recent graduate from SMU Full Stack certification program and persuing a career as a developer. I enjoy diving deep into many different technologies and strive to learn something new every day. I believe the things worth doing are worth over doing and have come to learn that pushing the limits is the only way to achieve great things!",
+  "address": "Dallas TX",
+  "website": "Alexiss.Diaz@live.com",
+  "education": [
+    {
+      "UniversityName": "SMU Full Stack Certification",
+      "specialization": "Some specialization",
+      "MonthOfPassing": "Mar",
+      "YearOfPassing": "2020",
+      "Achievements": "2020 Clear Technologies Hackathon Winner"
+    },
+    {
+      "UniversityName": "Eastfield Collage",
+      "specialization": "Computer Science",
+      "MonthOfPassing": "Current",
+      "YearOfPassing": "",
+      "Achievements": ""
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "AT&T",
+      "specialization": "Retail Sales Consultant",
+      "MonthOfLeaving": "",
+      "YearOfLeaving": "2020",
+      "Achievements": ""
+    },
+    {
+      "CompanyName": "T-mobile",
+      "specialization": "Mobile Associate",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2010",
+      "Achievements": ""
+    }
+  ],
+  "skillsDescription": "",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Javascript"
+    },
+    {
+      "skillname": "MYSQL"
+    },
+    {
+      "skillname": "Mongodb"
+    },
+    {
+      "skillname": "JQuery"
+    },
+    {
+      "skillname": "Bootstrap"
+    },
+    {
+      "skillname": "NodeJs"
+    },
+    {
+      "skillname": "Express"
+    },
+    {
+      "skillname": "Handelbarsjs"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "Project 1",
+      "description": "Web app",
+      "imgurl": "images/portfolio/weatherfy.png",
+      "link": "https://akunz19.github.io/Project-1/"
+    },
+    {
+      "name": "Project 2",
+      "description": "Web app",
+      "imgurl": "images/portfolio/petFetch.png",
+      "link": "https://app-project-2.herokuapp.com/"
+    },
+    {
+      "name": "Project 3",
+      "description": "Mobile app",
+      "imgurl": "images/portfolio/sentinelLogo.png",
+      "link": ""
+    },
+    {
+      "name": "Hackathon",
+      "description": "Web app",
+      "imgurl": "images/portfolio/alfredLogo.png",
+      "link": ""
+    },
+    {
+      "name": "Crystal Game",
+      "description": "Web app",
+      "imgurl": "images/portfolio/crystal_game.png",
+      "link": "https://akunz19.github.io/Project-1/"
+    },
+    {
+      "name": "Burger",
+      "description": "Web app",
+      "imgurl": "images/portfolio/burger_app.png",
+      "link": "https://pure-hamlet-00148.herokuapp.com/"
+    },
+    {
+      "name": "Friend Finder",
+      "description": "Mobile app",
+      "imgurl": "images/portfolio/friend_finder.png",
+      "link": "https://intense-basin-79942.herokuapp.com/"
+    },
+    {
+      "name": "Phychic Game",
+      "description": "Web app",
+      "imgurl": "images/portfolio/psychic_game.png",
+      "link": "https://alex05d.github.io/Psychic-Game/"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "When we set out to do a “hackathon” we had no idea what to expect. Let’s just say the participants in the hackathon were more impressive than we could have ever imagined.",
+      "name": "Visual Storage Intelligence"
+    }
+    // {
+    //   "description": "This is a sample testimonial",
+    //   "name": "Some technical guy"
+    // }
+  ]
+}
+
+export default resumeData
